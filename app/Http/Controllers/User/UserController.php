170d719a82b0be7e479600620f03c0f992b3c80d
Garrix <Garrix@GarrixdeMacBook-Pro.local>
2019-05-21 00:57:04 +0800

    add:[登入頁]Garrix-新增建立使用者頁面和API

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use App\Model\User\User As UserModle;
 use Alert;
 
@@ -26,16 +27,70 @@ class UserController extends Controller
 
         $aUserList = $_oUserModel
             ->where('user_name', $sUserName)
-            ->where('password', $sPassword)
             ->get()
-            ->toArray();
+            ->toArray()[0];
 
         ## 判斷如果無使用者
-        if (empty($aUserList)) {
-            Alert::error('錯誤', '帳號or密碼錯誤,請重新登入');
+        if (empty($aUserList) ||
+            !(Hash::check($sPassword, $aUserList['password']))) {
+            toast('帳號or密碼錯誤，請重新登入','error','top-right');
             return redirect()->back();
         }
 
-        return redirect('home')->with('success', 'Login Successfully!');
+        toast('登入成功','success','top-right');
+        return redirect('home');
+    }
+
+    /**
+     * 新增使用者
+     *
+     * @return boolean
+     */
+    public function createUser(
+        Request $_oRequest,
+        UserModle $_oUserModel
+    )
+    {
+        $aUserList = [];
+
+        $sUserName = $_oRequest->input('user_name');
+        $sPassword = $_oRequest->input('password');
+        $sRePassword = $_oRequest->input('re_password');
+
+        ## 簡易判斷
+        if (empty($sUserName)) {
+            toast('帳號不能為空白','error','top-right');
+            return redirect()->back();
+        } elseif (($sPassword !== $sRePassword) || empty($sPassword) || empty($sRePassword)) {
+            toast('密碼or確認密碼錯誤,請重新輸入','error','top-right');
+            return redirect()->back();
+        }
+
+        ## 密碼簡易加密
+        $sHashPassword = Hash::make($sPassword, [
+            'memory' => 1024,
+            'time' => 2,
+            'threads' => 2,
+        ]);
+
+        try {
+            $bResult = $_oUserModel->insert(
+                [
+                    'user_name' => $sUserName,
+                    'password' => $sHashPassword,
+                    'level' => 3
+                ]
+            );
+        } catch (\Exception $e) {
+            if ($e->getCode() === '23000') {
+                toast('使用者已存在','error','top-right');
+                return redirect()->back();
+            }
+            toast('新增失敗！！','error','top-right');
+            return redirect()->back();
+        }
+
+        toast('新增帳號成功，請重新登入!!','success','top-right');
+        return redirect('login');
     }
 }
