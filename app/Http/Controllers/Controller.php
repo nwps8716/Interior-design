7e67fa587070d10e30e3483cb5a876147f12d6f5
Garrix <Garrix@GarrixdeMacBook-Pro.local>
2019-05-25 14:39:15 +0800

    update:[工程單價]Garri-新增權限判斷

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,32 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * 檢查使用者權限
+     *
+     * @return boolean
+     */
+    public function checkSession(Request $_oRequest, $_bShowToVisitor)
+    {
+        $aLoginUser = $_oRequest->session()->get('login_user_info');
+
+        ## 判斷使用者已被登出
+        if (empty($aLoginUser)) {
+            toast('使用者已被登出！！','error','top-right');
+            return 'login';
+
+        ## 判斷使用者層級有無權限查看此頁面
+        } elseif ($aLoginUser['level'] === 3 && $_bShowToVisitor === false) {
+            toast('使用者無權限！！','error','top-right');
+            return 'home';
+        }
+
+        return 'success';
+    }
 }
