c9e28fdf0b1f39aa4bdea103e1010b9c80bf3679
lid_chen <lid_chen@CY0011553-LID-CHEN.local>
2019-05-25 18:58:04 +0800

    add:[坪數估價]第一版

--- app/Http/Controllers/Pings/PingsController.php
+++ app/Http/Controllers/Pings/PingsController.php
@@ -8,6 +8,14 @@ use Alert;
 
 class PingsController extends Controller
 {
+    private $level = [
+        1 => 'A',
+        2 => 'B',
+        3 => 'C',
+        4 => 'D',
+        5 => 'E',
+        6 => 'F'
+    ];
     /**
      * 主頁面
      *
@@ -19,6 +27,83 @@ class PingsController extends Controller
             toast('使用者已被登出！！','error','top-right');
             return redirect('login');
         }
-        return view('pings');
+
+        $aData = [];
+        $iDefault_pings = (int) $_oRequest->input('pings', 20); ## 坪數
+        $iEngineering_budget = 45; ## 工程預算
+        $iSystem_budget = 100 - $iEngineering_budget; ## 系統預算
+
+        foreach ($this->level as $key => $level_name) {
+            $aData[$key]['pings'] = $iDefault_pings;
+            $aData[$key]['level'] = $level_name;
+            $aData[$key]['price_of_level'] = $key * 100;
+            $aData[$key]['price'] = $iDefault_pings * $aData[$key]['price_of_level'];
+            $aData[$key]['engineering_budget'] = $iEngineering_budget . '%';
+            $aData[$key]['engineering_budget_total'] = $aData[$key]['price'] * ($iEngineering_budget/100);
+            $aData[$key]['system_budget'] = $iSystem_budget . '%';
+            $aData[$key]['system_price'] = $aData[$key]['price'] * ($iSystem_budget/100);
+            $aTmp = $this->countSystemCardAndDiscount($aData[$key]['system_price']);
+            $aData[$key]['system_card_price'] = $aTmp['card_price'];
+            $aData[$key]['system_discount'] = $aTmp['discount'];
+        }
+        
+        return view('pings', [
+            'default_pings' => $iDefault_pings,
+            'main_data' => $aData,
+        ]);
+    }
+    ## 使用系統售價來計算系統牌價、系統折數
+    private function countSystemCardAndDiscount($iSystemPrice)
+    {
+        $iCardPrice = 0;
+        $iDiscount = 0;
+        ## 系統牌價
+        switch (true) {
+            case (($iSystemPrice / 0.95) < 200000):
+                $iCardPrice = $iSystemPrice;
+                break;
+            case (($iSystemPrice / 0.65) >= 500000);
+                $iCardPrice = $iSystemPrice / 0.65;
+                break;
+            case (($iSystemPrice / 0.75) >= 400000);
+                $iCardPrice = $iSystemPrice / 0.75;
+                break;
+            case (($iSystemPrice / 0.85) >= 300000);
+                $iCardPrice = $iSystemPrice / 0.85;
+                break;
+            case (($iSystemPrice / 0.95) >= 200000);
+                $iCardPrice = $iSystemPrice / 0.95;
+                break;
+            default:
+                $iCardPrice = $iSystemPrice;
+                break;
+        }
+        ## 系統折數
+        switch (true) {
+            case ($iCardPrice < 200000):
+                $iDiscount = 0;
+                break;
+            case ($iCardPrice > 500000);
+                $iDiscount = 0.65;
+                break;
+            case ($iCardPrice > 400000);
+                $iDiscount = 0.75;
+                break;
+            case ($iCardPrice > 300000);
+                $iDiscount = 0.85;
+                break;
+            case ($iCardPrice > 200000);
+                $iDiscount = 0.95;
+                break;
+            default:
+                $iDiscount = 0;
+                break;
+        }
+
+
+        return [
+            'card_price' => round($iCardPrice, 2),
+            'discount' => $iDiscount,
+        ];
     }
 }
--- resources/views/pings.blade.php
+++ resources/views/pings.blade.php
@@ -1,15 +1,61 @@
 @extends('home')
 
 @section('feature')
-  <h1>坪數估價</h1>
-  <button id="test">hello</button>
+  <div class="pings-title">
+    <h1 class='content-title'>坪數估價</h1>
+    <button class="system-btn btn btn-primary">工程、系統預算修改</button>
+  </div>
+  <table class="table table-bordered table-striped pings-table">
+    <thead class="thead-dark">
+      <tr>
+        <th>實內坪數</th>
+        <th>坪數</th>
+        <th>等級</th>
+        <th>錢/每坪</th>
+        <th>價錢</th>
+        <th>工程預算%</th>
+        <th>工程總預算</th>
+        <th>系統預算%</th>
+        <th>系統售價</th>
+        <th>系統牌價</th>
+        <th>系統折數</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach($main_data as $id => $sub_data)
+        <tr>
+          @if($id === 1)
+            <td rowspan="11" class="pings-td">
+              <input id="pings-number" type="number" value="{{ $default_pings }}" min="1" max="10000"/>
+            </td>
+          @endif
+          @foreach($sub_data as $name => $detail_data)
+            <td class="{{$name}}">{{ $detail_data }}</td>
+          @endforeach
+        </tr>
+      @endforeach
+    </tbody>
+  </table>
 @endsection
 
 @section('script')
 @parent
 <script>
-  $("#test").click(function(e) {
-    alert(123);
+  $('#pings-number').keyup(function (e) {
+    this.value = this.value.replace(/[^0-9\.-]/g, '');
+  });
+  $('#pings-number').change(function (e) {
+    // var url = window.location.href;
+    if (this.value > 10000) {
+      this.value = 10000;
+    } else if (this.value < 1) {
+      this.value = 1;
+    }
+    window.location.href = window.location.origin + '/pings?pings=' + this.value;
+    // console.log(url);
+    // setTimeout(function () {
+    //   console.log(123);
+    // }, 100);
   });
 </script>
 @endsection
@@ -17,8 +63,25 @@
 @section('style')
 @parent
 <style>
-  #test {
-    color: red;
+  .pings-title {
+    position: relative;
+  }
+  .system-btn {
+    position: absolute;
+    top: 13px;
+    left: 160px;
+  }
+  .pings-table,
+  .level {
+    text-align: center;
+  }
+  tbody {
+    text-align: right;
+  }
+  .pings-td {
+    vertical-align: middle !important;
+    font-size: 24px;
+    text-align: center;
   }
 </style>
 @endsection
