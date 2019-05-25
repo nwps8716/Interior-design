e08a6b6ff409a003df6c4866c56018d229b645e1
Garrix <Garrix@GarrixdeMacBook-Pro.local>
2019-05-25 23:41:43 +0800

    add:[裝潢工程預算表]Garrix-新增第一版

--- app/Http/Controllers/Budget/BudgetController.php
+++ app/Http/Controllers/Budget/BudgetController.php
@@ -12,51 +12,89 @@ use Response;
 class BudgetController extends Controller
 {
     ## 級距
-    private $aSpacing = ['A', 'B', 'C', 'D', 'E', 'F'];
+    private $aSpacing = [
+        1 => 'A',
+        2 => 'B',
+        3 => 'C',
+        4 => 'D',
+        5 => 'E',
+        6 => 'F'
+    ];
 
     /**
      * 取得裝潢工程預算表
      *
      * @return boolean
      */
-    public function getEngineering(Request $_oRequest)
+    public function getEngineering(
+        Request $_oRequest,
+        EngineeringModle $_oEngineeringModle,
+        SubEngineeringModle $_oSubEngineeringModle
+    )
     {
+        $aResult = $aEngineeringList = [];
+        $iSubTotal = 0;
+        ## 測試數量
+        $iTestNum = 10;
+        $sTestRemark = 'GG';
+        $iTestTotal = 100000;
+
+        $iBudget = (int) $_oRequest->input('budget', 1);
+
         ## 判斷使用者權限
         if ($this->checkSession($_oRequest, false) !== 'success') {
             return redirect($this->checkSession($_oRequest, false));
         }
 
-        $iBudget = (int) $_oRequest->budget;
+        ## 取得工程主項目列表
+        $aEngineering = $_oEngineeringModle
+            ->get()
+            ->sortBy('sort')
+            ->pluck('project_name', 'project_id')
+            ->toArray();
 
+        ## 給預設Key值
+        foreach ($aEngineering as $key => $value) {
+            $aResult[$key] = [];
+        }
+
+        ## 取得工程子項目列表
+        $aSubEngineering = $_oSubEngineeringModle
+            ->get()
+            ->toArray();
+
+        ## 整理資料
+        foreach ($aSubEngineering as $iKey => $aValue) {
+            $aResult[$aValue['project_id']][] = [
+                'sub_project_id' => $aValue['sub_project_id'],
+                'sub_project_name' => $aValue['sub_project_name'],
+                'unti_price' => $aValue['unti_price'],
+                'unti' => $aValue['unti'],
+                'number' => $iTestNum,
+                'remark' => $sTestRemark
+            ];
+
+            $iSubTotal += ($iTestNum * $aValue['unti_price']);
+        }
+
+        $aTotalData = [
+            'total' => $iTestTotal,
+            'sub_total' => $iSubTotal,
+            'remaining_money' => ($iTestTotal - $iSubTotal),
+        ];
+
+        // echo "<pre> 取得裝潢工程預算表 getEngineering <pre>";
         // echo "<pre>";
-        // print_r($iBudget);
+        // print_r($aResult);
         // exit;
         return view('budget/engineering', [
-            'spacing' => $this->aSpacing
+            'spacing' => $this->aSpacing,
+            'budget_id' => $iBudget,
+            'total_info' => $aTotalData,
+            'engineering' => $aEngineering,
+            'list' => $aResult
         ]);
     }
-
-    // /**
-    //  * 取得裝潢工程預算表
-    //  *
-    //  * @return boolean
-    //  */
-    // public function getEngineering(Request $_oRequest)
-    // {
-    //     ## 判斷使用者權限
-    //     if ($this->checkSession($_oRequest, false) !== 'success') {
-    //         return redirect($this->checkSession($_oRequest, false));
-    //     }
-
-    //     $iBudget = (int) $_oRequest->budget;
-
-    //     // echo "<pre>";
-    //     // print_r($iBudget);
-    //     // exit;
-    //     return view('budget/engineering', [
-    //         'spacing' => $this->aSpacing
-    //     ]);
-    // }
 
     /**
      * 取得系統工程預算表
--- resources/views/Budget/engineering.blade.php
+++ resources/views/Budget/engineering.blade.php
@@ -1,85 +1,145 @@
 @extends('home')
 
 @section('feature')
-  <div>
-      <h1 class='content-title'>裝潢工程預算表</h1>
-      <div>
-        <form action="/engineering/budget" method="POST" class="budget-form" id="budget-form">
-          {{ csrf_field() }}
-          <div class="form-group">
-            <label class="control-label">級距:</label>
-            <select class="budget">
-              @foreach($spacing as $id => $name)
-                <option value="{{$id}}">{{$name}}級裝潢工程預算</option>
-              @endforeach
-            </select>
-              <input type="submit" name="search" id="search" class="search-btn btn btn-primary" value="查詢"/>
-          </div>
-        </form>
-      </div>
+<div>
+  <h1 class='content-title'>裝潢工程預算表</h1>
+  <div class="search-budget">
+    <div class="form-group">
+      <label class="control-label">級距:</label>
+      <select class="budget">
+        @foreach($spacing as $id => $name)
+          <option
+            value="{{$id}}"
+            @if ($id == $budget_id)
+              selected="selected"
+            @endif>
+            {{$name}}級裝潢工程預算
+          </option>
+        @endforeach
+      </select>
+      <input type="submit" name="search" id="search" class="search btn btn-primary" value="查詢"/>
+    </div>
   </div>
+</div>
 @endsection
 
 @section('content')
 <!-- 裝潢工程預算列表 -->
 <div class="row">
   <div class="col-sm-12">
-    {{-- @foreach($engineering as $id => $projectname)
-    <h4 class='project-title'>{{$projectname}}</h4>
     <table class="table table-striped">
-        <span>
-          <thead>
-            <tr class="subproject-title">
-              <td >項目</td>
-              <td >單價</td>
-              <td >單位</td>
-              <td colspan = 2>Actions</td>
-            </tr>
-          </thead>
-          @foreach($sub_engineering[$id] as $sub_name)
-            <tbody>
-              <td class="project-name">{{$sub_name['sub_project_name']}}</td>
-              <td class="unti_price">{{$sub_name['unti_price']}}</td>
-              <td class="unti">{{$sub_name['unti']}}</td>
-              <td>
-                <button
-                  class="edit-modal btn btn-info"
-                  data-id="{{$sub_name['sub_project_id']}}"
-                  data-name="{{$sub_name['sub_project_name']}}"
-                  data-unti="{{$sub_name['unti']}}"
-                  data-untiprice="{{$sub_name['unti_price']}}"
-                  data-projectname="{{$projectname}}">
-                    <span class="glyphicon glyphicon-edit">Edit</span>
-                </button>
-                <button
-                  class="delete-modal btn btn-danger"
-                  data-id="{{$sub_name['sub_project_id']}}"
-                  data-name="{{$sub_name['sub_project_name']}}"
-                  data-projectname="{{$projectname}}">
-                    <span class="glyphicon glyphicon-trash">Delete</span>
-                </button>
-              </td>
-            </tbody>
-          @endforeach
+      <span>
+        <thead>
+          <tr class="">
+            <td>{{$spacing[$budget_id]}}級裝潢工程總預算</td>
+            <td class="num-td">${{number_format($total_info['total'], 2)}}</td>
+            <td>小記</td>
+            <td class="num-td">${{number_format($total_info['sub_total'], 2)}}</td>
+            <td>剩餘預算</td>
+            <td class="num-td">
+              @if($total_info['remaining_money'] < 0)
+                <span style="color:red">
+                  ${{number_format($total_info['remaining_money'], 2)}}
+                </span>
+              @else
+                ${{number_format($total_info['remaining_money'], 2)}}
+              @endif
+            </td>
+          </tr>
+        </thead>
       </span>
-    </table><br>
-    @endforeach --}}
+    </table>
+    @foreach($engineering as $id => $projectname)
+      <h4 class='project-title'>{{$projectname}}</h4>
+      <table class="table table-striped">
+        <thead>
+          <tr class="subproject-title">
+            <td >項目</td>
+            <td >單價</td>
+            <td >數量</td>
+            <td >小記</td>
+            <td >尚餘空間</td>
+            <td >單位</td>
+            <td >備註</td>
+          </tr>
+        </thead>
+        @foreach($list[$id] as $sub_name)
+          <tbody>
+            <td class="project-name">{{$sub_name['sub_project_name']}}</td>
+            <td class="unti_price">{{$sub_name['unti_price']}}</td>
+            <td class="number">{{$sub_name['number']}}</td>
+            <td class="sub_total">
+              {{number_format($sub_name['unti_price'] * $sub_name['number'], 2)}}
+            </td>
+            <td class="free_space">
+              @if(number_format($total_info['remaining_money'] / $sub_name['unti_price'], 2) < 0)
+                <span style="color:red">
+                  {{number_format($total_info['remaining_money'] / $sub_name['unti_price'], 2)}}
+                </span>
+              @else
+                {{number_format($total_info['remaining_money'] / $sub_name['unti_price'], 2)}}
+              @endif
+            </td>
+            <td class="unti">{{$sub_name['unti']}}</td>
+            <td class="remark">{{$sub_name['remark']}}</td>
+          </tbody>
+        @endforeach
+      </table><br>
+    @endforeach
   <div>
 </div>
-
 @endsection
 
 @section('script')
 @parent
 <script>
+  // 查詢其他級距工程預算表
+  $('.search-budget').on('click', '.search', function() {
+    var budgetID = $(".budget").val();
+    window.location.href = window.location.origin + '/engineering/budget?budget=' + budgetID;
+  });
 </script>
 @endsection
 
 @section('style')
 @parent
 <style>
-.search-btn {
+.search {
   margin: 0px 0px 0px 10px;
+}
+.table th, .table td {
+  border: 1px solid #dee2e6;
+}
+.num-td {
+  background-color: #FFFFBF;
+}
+.project-title {
+  color: white;
+  background-color: #2d76be;
+  padding: 10px;
+  text-align: center;
+  margin-bottom: 0px;
+}
+.subproject-title {
+  background-color: #a6c7e8;
+}
+.project-name {
+  width: 40%;
+}
+.unti_price {
+  width: 10%;
+}
+.number {
+  width: 8%;
+}
+.sub_total {
+  width: 10%;
+}
+.free_space {
+  width: 12%;
+}
+.unti {
+  width: 8%;
 }
 </style>
 @endsection
